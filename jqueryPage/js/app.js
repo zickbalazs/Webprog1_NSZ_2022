20f69d542de026c2b3d60388481fcb7368df0b1d
zickbalazs <zickbalazs@turr.hu>
2022-05-16 09:03:42 +0200

    állatmenhely prototípus kész

--- jqueryPage/js/app.js
+++ jqueryPage/js/app.js
@@ -1,8 +1,3 @@
-async function contentLoad(filename, animal){
-    await $('#content').load('html/'+filename);
-    $('#animals').html(GetAnimals(animal));
-    
-}
 let animals = [
     {
         "type":"cat",
@@ -30,17 +25,68 @@ let animals = [
         "picture":"img/shalomander.png",
         "description":"Lorem ipsum dolor sit amet consictetudor adispiscing elit",
         "price": 25000
+    },
+    {
+        "type":"dog",
+        "name":"Doci",
+        "age":7,
+        "gender":"female",
+        "picture":"img/doci_raw.png",
+        "description":"Lorem ipsum dolor sit amet consictetudor adispiscing elit",
+        "price": 30000
+    },
+    {
+        "type":"dog",
+        "name":"Blöki",
+        "age":1,
+        "gender":"male",
+        "picture":"img/doci_raw.png",
+        "description":"Lorem ipsum dolor sit amet consictetudor adispiscing elit",
+        "price": 25000
     }
 ]
-
-function GetAnimals(type){
+let info = {
+    'address': '6500 Baja, Bácska tér 1.',
+    'email':'menhely@mail.com',
+    'phone':'06-23-12345678'
+}
+document.onload=contentLoad('main.html', '');
+function DisplayInfo(){
+    document.querySelector('#count').innerHTML = `Hirdetések száma: ${animals.length} db`;
+    document.querySelector('#info').querySelectorAll('p')[0].innerHTML=`<h5>Cím:</h5>${info.address}`;
+    document.querySelector('#info').querySelectorAll('p')[1].innerHTML=`<h5>E-Mail:</h5> ${info.email}`;
+    document.querySelector('#info').querySelectorAll('p')[2].innerHTML=`<h5>Telefon:</h5>${info.phone}`;
+}
+$('document').ready(()=>{
+    DisplayInfo();
+})
+function activeChange(to){
+    document.querySelector('.active').classList.toggle('active');
+    document.querySelector(to).classList.toggle('active');
+}
+function contentLoad(filename, animal){
+    $('#content').fadeOut(300, ()=>{
+        $('#content').load('html/'+filename, ()=>{
+            $('#animals').html(GetAnimals(animal));
+        });
+        $('#content').fadeIn();
+    });
+}
+function GetAnimals(animal){
     let div = document.createElement('div');
-    div.classList.add('row');
+    div.classList.add('container');
+    let row = document.createElement('div');
+    row.classList.add('row');
+    let i = 0;
     animals.forEach(e=>{
-        if (e.type==type){
+        if (i%3==0){
+            div.append(row);
+            row.innerHTML="";
+        }
+        if (e.type==animal){
             let card = document.createElement('div');
-            card.innerHTML=`<div class="card col-lg-4">
-                <img src=${e.picture} class="card-img-top" alt="">
+            card.classList.add('col-lg-4', 'gx-3','card')
+            card.innerHTML=`<img src=${e.picture} class="card-img-top" alt="">
                 <div class="card-body">
                     <h5 class="card-title">${e.name}</h5>
                     <p class="card-text">
@@ -49,11 +95,10 @@ function GetAnimals(type){
                             <li>Ár: ${e.price}</li>
                         </ul>${e.description}
                     </p>
-                </div>
-            </div>`;
-            div.append(card);
+                </div>`;
+            i++;
+            row.append(card);
         }
     })
     return div;
-    
 }
